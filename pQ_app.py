21974aad4df1ddb8b8cb8c2474fa5d410ce77f40
Nadir Sarigul <nadir.nyc62@gmail.com>
2021-08-04 15:31:25 -0400

    update1

--- pQ_app.py
+++ pQ_app.py
@@ -14,11 +14,17 @@ stan_cur_conc = np.array([1.5 , 1.  , 0.75, 0.5 , 0.25, 0.])
 
 def main():
     """protein guantification app streamlit"""
-    
+   
     st.title("protein guantification ")
     data = st.file_uploader("Upload Dataset", type = ["csv", "xlsx"])
+    
+    
     if data is not None:
-        df = pd.read_csv(data,delimiter=',', header=None, skiprows=1, names=['LABEL','1','2','3','4','5','6','7','8','9','10','11','12'])
+        df = pd.read_csv(data,delimiter=',', header=None, skiprows=1, 
+                         #names=['LABEL','1','2','3','4','5','6','7','8','9','10','11','12']
+                        )
+        df.columns = list(map(str, df.columns))
+        all(isinstance(column, str) for column in df.columns)
         st.dataframe(df)
         
         if st.checkbox("Show shape"):
@@ -39,85 +45,62 @@ def main():
             new_df = df[select_columns]
             st.dataframe(new_df)    
             ran = range(len(new_df))
+            
+            #st.write(new_df.columns)
             #calculate standart curve average
-            if st.checkbox("standart curve average"):
-                        stand_cur_Ave = []
-                        for col in range(len(new_df)):
-                            mean = (new_df['1'][col]+new_df['2'][col]+new_df['3'][col])/3
-                            stand_cur_Ave.append(mean)
-                            
+            #for i in range(len(new_df)):
+                #st.write(sum(new_df.loc[i])/3)
+                
+        #if st.checkbox("standart curve average"):
+            
+            stand_cur_Ave = []
+            for col in range(len(new_df)):
+                mean = sum(new_df.loc[col])/3
+                stand_cur_Ave.append(mean)
+
             stand_cur_Ave = np.array(stand_cur_Ave)
-            st.write(stand_cur_Ave)
+            #st.write(stand_cur_Ave)
             fig, ax = plt.subplots(figsize=(10,8))
             plt.scatter(stan_cur_conc, stand_cur_Ave)
             plt.xlabel("Standard Curve Concentration", fontsize = 14)
             plt.ylabel("Standard Curve Average",fontsize = 14)
-            st.pyplot(fig)
-        
-        #r_squared
-        if st.checkbox("r_squared"):
-            correlation_matrix = np.corrcoef(stan_cur_conc, stand_cur_Ave)
-            correlation_xy = correlation_matrix[0,1]
-            r_squared = correlation_xy**2
-            st.write("r_squared: ", r_squared)
-
-        if st.checkbox("data points"):
-            fig, ax = plt.subplots(figsize=(10,8))
+            
+            
+           
+            
+   
+            # fit a set of data points 
             plt.scatter(stan_cur_conc, stand_cur_Ave)
             m,b= np.polyfit(stan_cur_conc, stand_cur_Ave, 1)
             plt.plot(stan_cur_conc,m*stan_cur_conc+b)
             plt.xlabel("Standard Curve Concentration", fontsize = 14)
-            plt.ylabel("Standard Curve Average",fontsize = 14)
-            st.pyplot(fig)
-            st.write("m value: ", m)
-            st.write("b value: " ,b)
+            plt.ylabel("Standard Curve Average",fontsize = 14);
+            print("m value: ", m)
+            print("b value: " ,b)
             
-        #our sample
-        sample = df[['4','5','6']]
-        sample.rename(columns ={'4':"measurement1",'5':"measurement2",'6':"measurement3"}, inplace =True)
-
-        sample1 = df[['7','8','9']]
-        sample1.rename(columns ={'7':"measurement1",'8':"measurement2",'9':"measurement3"}, inplace =True)
-
-        sample2 = df[['10','11','12']]
-        sample2.rename(columns ={'10':"measurement1",'11':"measurement2",'12':"measurement3"}, inplace =True)
-        our_sample = [sample, sample1, sample2]
-        our_sample = pd.concat(our_sample) 
-        #st.dataframe(our_sample)  
-        
-        #reset index so that each sample has a unique identifier
-
-        our_sample.reset_index(drop=True, inplace = True)
-        our_sample.index = our_sample.index.set_names(['Sample'])
-        st.dataframe(our_sample)    
+            #stan_cur_conc = np.array([1.5 , 1.  , 0.75, 0.5 , 0.25, 0.])
+            correlation_matrix = np.corrcoef(stan_cur_conc, stand_cur_Ave)
+            correlation_xy = correlation_matrix[0,1]
+            r_squared = correlation_xy**2
+            #st.pyplot(fig)
+        with st.form(key = "form"):
+            column1, column2, column3 = st.beta_columns(3)
+            with column1:
+                submit = st.form_submit_button(label = "Standard Curve Average")
+            with column2:
+                submit1 = st.form_submit_button(label = "R_Squrared")
+              
+            with column3:
+                submit2 = st.form_submit_button(label = "Standard Curve Graphic1")
+            if submit:
+                st.write(stand_cur_Ave)
+            if submit1:
+                st.write(r_squared)
+                
             
-        measurement_average = []
-
-        for sample in range(len(our_sample)):
-            mean = (our_sample["measurement1"][sample]+our_sample["measurement2"][sample]+our_sample["measurement3"][sample])/3
-
-            measurement_average.append(mean)
-        measurement_average = np.array(measurement_average)
-        st.write(measurement_average)
-        
-        
-        def get_measurement_conc(y):
-            protein_conc= []
-            update_dilution = []
-            for protein in y:
-                protein_conc.append(round((protein-b)/m ,2))
-
-                update_dilution.append(protein_conc*4)
-                df1 = {"measurement_average":measurement_average,"protein_conc":protein_conc}
-            return pd.DataFrame(df1)
-        st.dataframe(get_measurement_conc(measurement_average))
-  
-
-
-        if st.checkbox("Show Summary"):
-            st.write(df.describe())
-        
-    
-    
+            if submit2:
+                st.pyplot(fig)
+            
+            
 if __name__ =="__main__":
     main()
