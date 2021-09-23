c4d3c908b7b3a9fcac3b6acd93dc2399b4ce9b4a
Nadir Sarigul <nadir.nyc62@gmail.com>
2021-09-22 21:24:00 -0400

    update

--- pQ_app.py
+++ pQ_app.py
@@ -9,14 +9,20 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib
 matplotlib.use("Agg")
-
+#st.image("gomes1.png")
 stan_cur_conc = np.array([1.5 , 1.  , 0.75, 0.5 , 0.25, 0.])
 
 def main():
     """protein guantification app streamlit"""
+    
    
-    st.title("protein guantification ")
+    st.image("gomes1.png")
+    
+    st.title("Protein Quantification ")
     data = st.file_uploader("Upload Dataset", type = ["csv", "xlsx"])
+    st.subheader("Please set up your quantification plate as shown below. Your samples should be set up in rows with replicas in columns")
+    st.image("gomes2.png")
+    
     
     
     if data is not None:
@@ -27,71 +33,52 @@ def main():
         all(isinstance(column, str) for column in df.columns)
         st.dataframe(df)
         
-        if st.checkbox("Show shape"):
-            st.write(df.shape)
-        #if st.checkbox("Show Columns"):
-            #all_columns = df.columns.to_list()
-            #st.write(all_columns)
-            
+        #Select Columns for Standard Curve Calculated
         #if st.checkbox("Select Columns to Show"):
-            #all_columns = df.columns.to_list()
-            #select_columns = st.multiselect("Select Columns", all_columns)
-            #new_df = df[select_columns]
-            #st.dataframe(new_df)
-            
-        if st.checkbox("Select Columns to Show"):
-            all_columns = df.columns.to_list()
-            select_columns = st.multiselect("Select Columns", all_columns)
-            new_df = df[select_columns]
-            st.dataframe(new_df)    
-            ran = range(len(new_df))
-            
-            #st.write(new_df.columns)
-            #calculate standart curve average
-            #for i in range(len(new_df)):
-                #st.write(sum(new_df.loc[i])/3)
-                
-        #if st.checkbox("standart curve average"):
-            
-            stand_cur_Ave = []
-            for col in range(len(new_df)):
-                mean = sum(new_df.loc[col])/3
-                stand_cur_Ave.append(mean)
+        st.success("Standard Curve Calculations")
+        all_columns = df.columns.tolist()
+        select_columns = st.multiselect("Select Columns for Standard Curve", all_columns)
+        new_df = df[select_columns]
+        select_columns_len = len(select_columns)
+        st.sidebar.image("gomes.png")
 
-            stand_cur_Ave = np.array(stand_cur_Ave)
-            #st.write(stand_cur_Ave)
-            fig, ax = plt.subplots(figsize=(10,8))
-            plt.scatter(stan_cur_conc, stand_cur_Ave)
-            plt.xlabel("Standard Curve Concentration", fontsize = 14)
-            plt.ylabel("Standard Curve Average",fontsize = 14)
+
+
+        stand_cur_Ave = []
+        for col in range(len(new_df)):
+            mean = sum(new_df.loc[col])/3#select_columns_len
+            stand_cur_Ave.append(mean)
+
+        stand_cur_Ave = np.array(stand_cur_Ave)
+        #st.write(stand_cur_Ave)
+
+        # fit a set of data points 
+        fig, ax = plt.subplots(figsize=(10,8))
+        plt.scatter(stan_cur_conc, stand_cur_Ave)
+        m,b= np.polyfit(stan_cur_conc, stand_cur_Ave, 1)
+        plt.plot(stan_cur_conc,m*stan_cur_conc+b)
+        plt.xlabel("Standard Curve Concentration", fontsize = 14)
+        plt.ylabel("Standard Curve Average",fontsize = 14);
+        print("m value: ", m)
+        print("b value: " ,b)
+        #st.pyplot(fig)
+
+
+        #stan_cur_conc = np.array([1.5 , 1.  , 0.75, 0.5 , 0.25, 0.])
+        correlation_matrix = np.corrcoef(stan_cur_conc, stand_cur_Ave)
+        correlation_xy = correlation_matrix[0,1]
+        r_squared = correlation_xy**2
+        #st.pyplot(fig)
             
-            
-           
-            
-   
-            # fit a set of data points 
-            plt.scatter(stan_cur_conc, stand_cur_Ave)
-            m,b= np.polyfit(stan_cur_conc, stand_cur_Ave, 1)
-            plt.plot(stan_cur_conc,m*stan_cur_conc+b)
-            plt.xlabel("Standard Curve Concentration", fontsize = 14)
-            plt.ylabel("Standard Curve Average",fontsize = 14);
-            print("m value: ", m)
-            print("b value: " ,b)
-            
-            #stan_cur_conc = np.array([1.5 , 1.  , 0.75, 0.5 , 0.25, 0.])
-            correlation_matrix = np.corrcoef(stan_cur_conc, stand_cur_Ave)
-            correlation_xy = correlation_matrix[0,1]
-            r_squared = correlation_xy**2
-            #st.pyplot(fig)
-        with st.form(key = "form"):
+        with st.form(key="form"):
             column1, column2, column3 = st.beta_columns(3)
             with column1:
-                submit = st.form_submit_button(label = "Standard Curve Average")
+                submit = st.form_submit_button(label = "Average Absorbance")
             with column2:
                 submit1 = st.form_submit_button(label = "R_Squrared")
-              
             with column3:
-                submit2 = st.form_submit_button(label = "Standard Curve Graphic1")
+                submit2 = st.form_submit_button(label = "Standard Curve Graphic")
+                
             if submit:
                 st.write(stand_cur_Ave)
             if submit1:
@@ -101,19 +88,21 @@ def main():
             if submit2:
                 st.pyplot(fig)
                 
-                
+            #sample calculations
+        st.success("Sample Calculations")
         col1, col2 = st.beta_columns(2)
+
         with col1:
-            
-            with st.beta_expander("Select Columns for calculate"):
+            with st.beta_expander("Select Columns for Sample Replicates"):
                 df1 = df.drop(columns = new_df)
                 all_column = df1.columns.to_list()
 
-                select_column = st.multiselect("Select Column", all_column)
-                new_df1 = df[select_column]
+
+                select_columns = st.multiselect(" ", all_column)
+                new_df1 = df[select_columns]
                 st.dataframe(new_df1)
         with col2:
-            with st.beta_expander("sample"):
+            with st.beta_expander("Avarage Absorbace"):
                 sample_Ave = []
                 for col in range(len(new_df1)):
                     mean = sum(new_df1.loc[col])/len(new_df1)
@@ -132,13 +121,16 @@ def main():
                 df1 = {"protein_conc":protein_conc}
                 md = pd.DataFrame(df1)
                 st.write(md)
-            with st.beta_expander("dilution"): 
+            #with st.beta_expander("dilution"): 
                 dilution = st.slider("update dilution")
                 update_dilution = (md*dilution)
                 
                 st.write(update_dilution)
+                    
+                
+               
             
-           
+            
                    
             
             
